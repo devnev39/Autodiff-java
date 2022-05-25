40e55a0d26b3ab5f6d780d36f667113cfb6333cd
Dhruv Bonde <devnev39@gmail.com>
2022-05-25 18:46:46 +0530

    test changes update

--- Main.java
+++ Main.java
@@ -25,7 +25,26 @@ public class Main {
         // Node g = Gradient.GradwrtVar(f, var_x);
         // System.out.println(g.operation().Representation());
         // System.out.println(g.Evaluate(null));
-        
+
+        // x^2 + 75x^3 - sin(x^5)
+
+        Node a1 = Node.pow(var_x,new Node(2.0f));
+        Node a2 = Node.pow(var_x,new Node(3.0f));
+        Node a3 = Node.mul(new Node(75.0f), a2);
+        Node a4 = Node.pow(var_x, new Node(5.0f));
+        Node a5 = Node.sin(a4);
+
+        Node a6 = Node.sum(a1, a3);
+        Node f = Node.sub(a6, a5);
+        System.out.println("    ");
+        System.out.println(f.operation().Representation());
+        Node g = Gradient.GradwrtVar(f, var_x);
+        System.out.println("    ");
+        System.out.println(g.operation().Representation());
+        System.out.println("    ");
+
+
+
         // Node a1 = Node.pow(var_x,new Node(3.0f));
         // Node a2 = Node.mul(var_x, new Node(4.0f));
         // Node a3 = Node.mul(a2, var_y);
@@ -35,13 +54,10 @@ public class Main {
         // Node a7 = Node.sum(a1, a4);
         // Node f = Node.sum(a7,a6);
 
-        // System.out.println(f.operation().Representation());
-        // // System.out.println(f.Evaluate(new Hashtable<String,Float>(){{
-        // //     put("x",3.0f);
-        // //     put("y",3.0f);
-        // //     put("z",3.0f);
-        // //     put("u",3.0f);
-        // // }}));
+        
+        System.out.println(f.Evaluate(new Hashtable<String,Float>(){{
+            put("x",3.0f);
+        }}));
 
         // System.out.println(f.printGraph());
         // System.out.println(f.Evaluate(null));
@@ -49,19 +65,19 @@ public class Main {
         // System.out.println(g.operation().Representation());
         // //System.out.println(g.printGraph());
         //System.out.println(f.Evaluate(null));
-        String eq = "( x^4 + 4x ) | x";
-        // // // printArray(eq.split("\\|"));
-        try {
-            Equation.processInput(eq);
-            String e = eq.split("\\|")[0];
-            Node n = Equation.createGraph(e.split("\\s"), 0, null);
-            System.out.println(n.operation().Representation());
-            System.out.println(n.Evaluate(null));
-            System.out.println(n.printGraph());
-            System.out.println(Gradient.GradwrtVar(n, Equation.varNodes(eq)).operation().Representation());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // String eq = "( x^4 + 4x ) | x";
+        // // // // printArray(eq.split("\\|"));
+        // try {
+        //     Equation.processInput(eq);
+        //     String e = eq.split("\\|")[0];
+        //     Node n = Equation.createGraph(e.split("\\s"), 0, null);
+        //     System.out.println(n.operation().Representation());
+        //     System.out.println(n.Evaluate(null));
+        //     System.out.println(n.printGraph());
+        //     System.out.println(Gradient.GradwrtVar(n, Equation.varNodes(eq)).operation().Representation());
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
         // String vars = Equation.removeSeparator(eq.split(" | "), "|")[0];
         // System.out.println(vars);
         // if(vars.contains(Operation.Ops().get(Operations.LOG))){
